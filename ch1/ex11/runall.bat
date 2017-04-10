43ab0575e3a58c2cd03e8cdb6449a2141a78765f
Takumi Miyagawa <takku.333@gmail.com>
2017-04-11 07:18:59 +0900

    ad ex11

---
+++ ch1/ex11/runall.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d %~dp0
+
+go run fetchall.go http://www.alexa.com/siteinfo/google.com http://www.alexa.com/siteinfo/youtube.com http://www.alexa.com/siteinfo/facebook.com http://www.alexa.com/siteinfo/baidu.com http://www.alexa.com/siteinfo/wikipedia.org http://www.alexa.com/siteinfo/yahoo.com http://www.alexa.com/siteinfo/reddit.com http://www.alexa.com/siteinfo/google.co.in http://www.alexa.com/siteinfo/qq.com http://www.alexa.com/siteinfo/taobao.com http://www.alexa.com/siteinfo/amazon.com
